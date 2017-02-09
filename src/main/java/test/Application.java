f9c90e04de0a8e93aa39cd58546a3aa038877576
praveenensrikumaran <praveenen24@yahoo.com>
2017-02-09 10:14:51 -0500

    Commiting everything lol

--- src/main/java/test/Application.java
+++ src/main/java/test/Application.java
@@ -8,7 +8,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication
+/*@SpringBootApplication
 public class Application {
 
     private static final Logger log = LoggerFactory.getLogger(Application.class);
@@ -66,4 +66,4 @@ public class Application {
         };
     }
 
-}
+}*/
