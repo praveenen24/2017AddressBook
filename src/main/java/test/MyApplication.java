585af278a331aed94fcea894b052c47c1659c5a1
praveenensrikumaran <praveenen24@yahoo.com>
2017-02-09 10:56:34 -0500

    Making changes for heroku (2)

--- src/main/java/test/MyApplication.java
+++ src/main/java/test/MyApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * Created by praveenensrikumaran on 2/2/2017.
  */
 @SpringBootApplication
-public class MyApplication {
+public class    MyApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MyApplication.class, args);
